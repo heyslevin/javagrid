1b057acf4ea10438b9d0a7490e0982d8940db6a0
memellaguno <memellaguno@gmail.com>
2019-12-26 16:31:19 -0600

    Grid Java Loop Setup

---
+++ script.js
@@ -0,0 +1,14 @@
+let texttest = "one ";
+let string = "two ";
+let newstring = 1;
+squares = 20;
+
+const container = document.querySelector(".gridContainer");
+const squareSelect = document.querySelector(".square");
+
+for (let i=0; i < squares; i++){
+	var squareCreate = document.createElement("div");
+	squareCreate.classList.add("square", "flexItem");
+
+	container.appendChild(squareCreate);
+}
