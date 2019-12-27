921983069bd847730122f7f9120aa05c10f13343
memellaguno <memellaguno@gmail.com>
2019-12-27 17:00:53 -0600

    Clear Grid working

--- script.js
+++ script.js
@@ -7,12 +7,18 @@ const container = document.querySelector(".gridContainer");
 
 //Create Grid
 
+function gridCreator(){
+
 for (let i=0; i < squares; i++){
 	var squareCreate = document.createElement("div");
 	squareCreate.classList.add("square", "flexItem");
 
 	container.appendChild(squareCreate);
 }
+
+}
+
+gridCreator();
 
 //Painter functions
 
@@ -24,19 +30,21 @@ function paintBlack(item) {
 //Clear Button
 
 let button = document.querySelector("button");
+let userinput;
 function clearcells() {
 	for (let i = 0; i < squares; i++) {
 		squarepicker[i].classList.remove("fillblack");
 	}	
+
+	squares = prompt("How many cells?");
+	document.querySelectorAll(".square").forEach(e => e.remove());
+
+	gridCreator();
+
 }
+
 button.addEventListener("click",clearcells);
 
-
-function clear(e){
-	e.target.classList.remove("paintBlack");
-}
-
-button.addEventListener("click",clearcells);
 //Event Listener for squares
 
 let squarepicker = document.querySelectorAll(".square");
