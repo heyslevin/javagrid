70d155f31910d5127bda77af990af32196552184
memellaguno <memellaguno@gmail.com>
2019-12-27 12:45:40 -0600

    Black fill works on hover

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ let newstring = 1;
 squares = 256;
 
 const container = document.querySelector(".gridContainer");
-const squareSelect = document.querySelector(".square");
 
 for (let i=0; i < squares; i++){
 	var squareCreate = document.createElement("div");
 	squareCreate.classList.add("square", "flexItem");
 
 	container.appendChild(squareCreate);
+}
+
+function paintBlack(item) {
+	//item.classList.add("fillblack");//
+	item.classList.add("fillblack");
+
+}
+
+let squarepicker = document.querySelectorAll(".square");
+let squaretest = document.querySelector(".square");
+
+squaretest.addEventListener("mouseenter",paintBlack);
+
+// squaretest.classList.add("fillblack");
+
+
+
+for (let i=0;i < squares; i++) {
+	squarepicker[i].addEventListener("mouseenter", function(){
+		squarepicker[i].classList.add("fillblack");
+	});
 }
